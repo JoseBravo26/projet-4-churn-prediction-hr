61deb6f674c00431a684b44840eb907cfe6930e8
Jose <bravocastanedajose@gmail.com>
2026-03-14 12:44:28 +0100

    test

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
 
-#test
 # ========================================
 # 📦 CHARGER MODÈLE, SCALER ET SEUIL
 # ========================================
